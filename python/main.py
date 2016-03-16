3d303ba5e77622de46ec70ba52ae6e26e6bc973e
Doug Murray <douglass.murray@gmail.com>
2016-03-16 19:14:06 -0400

    Added main.py script to start modularizing stock analysis system.  Main.py will be the script that is intermediary to the user and start-stock.py or any processing script.

---
+++ python/main.py
@@ -0,0 +1,9 @@
+# main.py
+#!/usr/bin/env python27
+"""Analysis of stocks using various methods, including ADL, MACD, etc.
+
+   User inputs stock symbol and desired analysis method(s).  
+
+   Author:  Douglass Murray
+   Date:  2016-03-16
+"""
